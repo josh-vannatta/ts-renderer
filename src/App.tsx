7ebd2fc659fcee9a2fec524e4e5a2c2399049d3b
josh-vannatta <jjvannatta88@gmail.com>
2024-07-15 23:47:50 -0600

    tested example app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import { ExampleApp } from './example/ExampleApp'
 
 function App() {
-
   return (
     <div style={{ display: "flex"}}>
       <div style={{ width: "50%", height: "100%"}}>
